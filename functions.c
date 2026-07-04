d62bcf9fb40a77851c169204d5f8a400b1ab9759
Yasin razmjo <yrz51081@gmail.com>
2026-07-04 08:44:26 -0700

    feat: add gcd function Closes #6

--- functions.c
+++ functions.c
@@ -5,7 +5,18 @@ int factorial(int n) { return 0; }
 int reverseNumber(int n) { return 0; }
 int sumDigits(int n) { return 0; }
 int countDigits(int n) { return 0; }
-int gcd(int a, int b) { return 0; }
+int gcd(int a, int b) {
+    if (a < 0) a = -a;
+    if( b < 0) b = -b;
+
+    while(b != 0) {
+        int reminder = a % b;
+        a = b;
+        b = reminder;
+    }
+    
+     return a; 
+    }
 int lcm(int a, int b) { return 0; }
 void reverseString(char str[]) {}
 int isOdd(int n) { return 0; }
