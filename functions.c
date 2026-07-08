df4ad9d7b54542871b913212c408daca51b6b73a
Fatemeh bitab <fatemehbitab039@gmail.com>
2026-07-08 14:49:40 -0700

    Add factorial function - closes #2

--- functions.c
+++ functions.c
@@ -1,52 +1,8 @@
-#include "functions.h"
-
-int isPrime(int n) { return 0; }
-int factorial(int n) { return 0; }
-int reverseNumber(int n) { return 0; }
-int sumDigits(int n) { return 0; }
-
-int countDigits(int n) 
-{
-    int count = 0;
-    if (n == 0)
-        return 1;
-    if (n < 0)
-        n = -n;
-    while (n != 0)
-    {
-        count++;
-        n /= 10;
+int factorial(int n) {
+    if (n < 0) return 0;
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
     }
-    return count;
+    return result;
 }
-
-int gcd(int a, int b) {
-    if (a < 0) a = -a;
-    if( b < 0) b = -b;
-
-    while(b != 0) {
-        int reminder = a % b;
-        a = b;
-        b = reminder;
-    }
-    
-     return a; 
-    }
-void reverseString(char str[]) {}
-int isOdd(int n) { return 0; }
-int isEven(int n)
-{
-    return n % 2 == 0;
-}
-int power(int base, int exp) { return 0; }
-int lcm(int a, int b) {
-    int max = (a > b) ? a : b;
-    while (1) {
-        if (max % a == 0 && max % b == 0) {
-            return max;
-        }
-        max++;
-    }
-}
-
-
